21eb05f26380c2e54ea9b9f679df9653a41c936b
Inshanemane <Inshanemane@users.noreply.github.com>
2014-10-31 13:16:43 -0700

    5
    Need to index and position form letters and then pop and replace in unknown

--- Hangmang.py
+++ Hangmang.py
@@ -1,31 +1,40 @@
 #HANGMANG: THE VIDEO GAME 2K14
 
+
+#You have guessed 
+#Letters: a b c etc
+
+#LETTERS
 letters1 = []
 letters2 = []
 letters3 = []
+#SPACE FOR 2 WORDS
 space=[" "]
+#UNDERSCORE
 unknown = []
+#NUMBER CHANGES CHANCES
 difficulty = []
-guessnumber = []
+
+guessnum = []
 
 
 
-
+#FUNCTION FOR DIFFICULTY CHANGE
 def difficulty():
     global diff
     for i in range(1):
-    	diff=int(input("What difficulty would you like? 1 = Easy, 2 = Medium, 3 = Hard"))
+        diff=int(input("What difficulty would you like? 6 = Easy, 5 = Medium, 4 = Hard"))
 
         
-
+#FUNCTION THAT GRABS WORDS FROM P1
 def new_words():
-    global n, wr1 ,wr2 ,letters1, letters2,letters3
+    global o, wr1 ,wr2 ,letters1, letters2,letters3
     
     
-    n=int(input("Number of words Player 1 would like to input"))	
+    o=int(input("Number of words Player 1 would like to input"))	
     
     for i in range(1):
-        if n == 2:
+        if o == 2:
             wr1=input("The word1")
             letters1.extend(wr1)
             wr2=input("The word2")
@@ -37,7 +46,7 @@ def new_words():
             
      
         
-        if n == 1:
+        if o == 1:
             wr1=input("The word1")
             letters1.extend(wr1)
             print letters1
@@ -45,31 +54,28 @@ def new_words():
     
     
 def hide_words():
-    global n, letters1, letters2
+    global unknown, o, letters1, letters2
     
-    
-    for i in range(len(letters1)):
-        unknown.append("_")
-    
-    for p in range(len(space)):
-        unknown.append(" ")
-    
-    
-    for z in range(len(letters2)):
-        unknown.append("_")
-
+    if o == 2:
+        for i in range(len(letters1)):
+            unknown.append("_")
+        for p in range(len(space)):
+            unknown.append(" ")
+        for z in range(len(letters2)):
+            unknown.append("_")
         
+    if  o == 1:
+        for i in range(len(letters1)):
+            unknown.append(" ")
+                           
         
-    
     print unknown
-
-
     
 def reveal_letter():
-    for i in range():
-        input("What is your guess?")
-    
-            
+    n=input("What is your guess?")
+    if n in wr1:
+        print "gj"
+#Have it index form letters and then remove and replace unknown            
     
     
     
@@ -80,8 +86,4 @@ def play_game():
     hide_words()
     reveal_letter()
 
-play_game()        
-        
-        
-        
-        
+play_game()
